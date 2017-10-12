24d94b907d3542e7968c4e410758b9a06f833a43
Verena <Verena@LAPTOP-T6V1EL0O.hfu-open.fu.hs-furtwangen.de>
2017-10-12 16:39:47 +0200

    save

--- aufgabe0/aufgabe0.js
+++ aufgabe0/aufgabe0.js
@@ -2,7 +2,7 @@ window.addEventListener("load", popup);
 var person = prompt("Gib deinen Namen ein");
 function popup() {
     if (person != null) {
-        document.getElementById("hallo").innerHTML = "Hallo " + person + "wie geht es dir?";
+        document.getElementById("hallo").innerHTML = "Hallo " + person + " wie geht es dir?";
     }
 }
 //# sourceMappingURL=aufgabe0.js.map
--- aufgabe0/aufgabe0.ts
+++ aufgabe0/aufgabe0.ts
@@ -4,6 +4,6 @@ var person: any = prompt("Gib deinen Namen ein");
 
 function popup(): void {
 if (person!= null) {
-    document.getElementById("hallo").innerHTML = "Hallo " + person + "wie geht es dir?";
+    document.getElementById("hallo").innerHTML = "Hallo " + person + " wie geht es dir?";
     }
 }
