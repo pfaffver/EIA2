0954f94b09874903eb1021691008a38cc9c51589
Verena <Verena@LAPTOP-T6V1EL0O>
2017-11-30 18:21:26 +0100

    aufgabe7

---
+++ aufgabe7/aufgabe7.js
@@ -0,0 +1,30 @@
+var StudiVZ;
+(function (StudiVZ) {
+    var students = [];
+    var stop = false;
+    while (!stop) {
+        var action = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+        switch (action) {
+            case "n":
+            case "N":
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+    function saveData(_input) {
+        return "Hier fehlt noch der richtige Code...";
+    }
+    function queryData(_matrikel) {
+        return "Hier fehlt noch der richtige Code...";
+    }
+})(StudiVZ || (StudiVZ = {}));
+//# sourceMappingURL=aufgabe7.js.map
---
+++ aufgabe7/aufgabe7.ts
@@ -0,0 +1,34 @@
+namespace StudiVZ {
+    interface StudentData {
+        // hier ist noch die richtige Datenstruktur festzulegen
+    }
+    var students: StudentData[] = [];
+    var stop: boolean = false;
+
+    while (!stop) {
+        var action: string = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+
+        switch (action) {
+            case "n":
+            case "N":
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel: number = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+
+    function saveData(_input: string): string {
+        return "Hier fehlt noch der richtige Code...";
+    }
+    function queryData(_matrikel: number): string {
+        return "Hier fehlt noch der richtige Code...";
+    }
+}
