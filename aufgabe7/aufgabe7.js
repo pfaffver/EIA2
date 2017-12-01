12c510738f244b2d27e7410469e7a12cc8647e45
Verena <Verena@LAPTOP-T6V1EL0O.hfu-open.fu.hs-furtwangen.de>
2017-12-01 16:15:13 +0100

    aghjgahjaj

--- aufgabe7/aufgabe7.js
+++ aufgabe7/aufgabe7.js
@@ -7,7 +7,7 @@ var StudiVZ;
         switch (action) {
             case "n":
             case "N":
-                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 = männlich oder 1 = weiblich) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -21,10 +21,50 @@ var StudiVZ;
         }
     }
     function saveData(_input) {
-        return "Hier fehlt noch der richtige Code...";
+        let res = _input.split(",", 6); // var input Eingabe wir durch die "," getrennt + 6 Eingabem�glichkeiten
+        if (parseInt(res[0]) == NaN) {
+            return "Bitte gebe eine Zahl ein!";
+        }
+        else if ((res[1]) == "") {
+            return "Bitte gebe deinen Namen ein!";
+        }
+        else if ((res[2]) == "") {
+            return "Bitte gebe einen Vornamen ein!";
+        }
+        else if (parseInt(res[3]) == NaN) {
+            return "Bitte gebe eine Zahl ein!";
+        }
+        else if (parseInt(res[4]) != 0 && parseInt(res[4]) != 1) {
+            return "Bitte gebe dein Geschlecht ein!";
+        }
+        else if ((res[5]) == "") {
+            return "Bitte gebe ein Kommentar ein!";
+        }
+        else {
+            var einStudent = {
+                matrikelnummer: parseInt(res[0]),
+                name: res[1],
+                vorname: res[2],
+                alter: parseInt(res[3]),
+                geschlecht: parseInt(res[4]) == 1,
+                kommentar: res[5]
+            };
+            students.push(einStudent);
+            console.log(students);
+            return "Gespeichert";
+        }
     }
     function queryData(_matrikel) {
-        return "Hier fehlt noch der richtige Code...";
+        for (var i = 0; i < students.length; i++) {
+            if (students[i].matrikelnummer == _matrikel) {
+                let geschlechtAusgabe;
+                if (students[i].geschlecht)
+                    geschlechtAusgabe = "weiblich";
+                else
+                    geschlechtAusgabe = "männlich";
+                return students[i].matrikelnummer + " " + students[i].name + " " + students[i].vorname + " " + students[i].alter + " " + geschlechtAusgabe + " " + students[i].kommentar;
+            }
+        }
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=aufgabe7.js.map
--- aufgabe7/aufgabe7.ts
+++ aufgabe7/aufgabe7.ts
@@ -5,9 +5,10 @@ namespace StudiVZ {
         vorname: string;
         alter: number;
         geschlecht: boolean;
-        kommentar: string;        
+        kommentar: string;
         // Datenstruktur festgelegt
     }
+
     var students: StudentData[] = [];
     var stop: boolean = false;
 
@@ -17,7 +18,7 @@ namespace StudiVZ {
         switch (action) {
             case "n":
             case "N":
-                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 = männlich oder 1 = weiblich) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -32,9 +33,56 @@ namespace StudiVZ {
     }
 
     function saveData(_input: string): string {
-        return "Hier fehlt noch der richtige Code...";
+        let res = _input.split(",", 6); // var input Eingabe wir durch die "," getrennt + 6 Eingabem�glichkeiten
+
+        if (parseInt(res[0]) == NaN)
+        { return "Bitte gebe eine Zahl ein!" }
+
+        else if ((res[1]) == "")
+        { return "Bitte gebe deinen Namen ein!" }
+
+        else if ((res[2]) == "")
+        { return "Bitte gebe einen Vornamen ein!" }
+
+        else if (parseInt(res[3]) == NaN)
+        { return "Bitte gebe eine Zahl ein!" }
+
+        else if (parseInt(res[4]) != 0 && parseInt(res[4]) != 1)
+        { return "Bitte gebe dein Geschlecht ein!" }
+
+        else if ((res[5]) == "")
+        { return "Bitte gebe ein Kommentar ein!" }
+
+        else {
+            var einStudent: StudentData = {
+                matrikelnummer: parseInt(res[0]),
+                name: res[1],
+                vorname: res[2],
+                alter: parseInt(res[3]),
+                geschlecht: parseInt(res[4]) == 1, //Da 1 abgespeichert ist (als right), ist 0 automatisch false!
+                kommentar: res[5]
+            }
+
+            students.push(einStudent);
+            console.log(students);
+            return "Gespeichert";
+        }
     }
+
     function queryData(_matrikel: number): string {
-        return "Hier fehlt noch der richtige Code...";
+        for (var i: number = 0; i < students.length; i++) {
+            if (students[i].matrikelnummer == _matrikel) {
+
+                let geschlechtAusgabe: string;
+
+                if (students[i].geschlecht)
+                    geschlechtAusgabe = "weiblich";
+                else
+                    geschlechtAusgabe = "männlich";
+
+                return students[i].matrikelnummer + " " + students[i].name + " " + students[i].vorname + " " + students[i].alter + " " + geschlechtAusgabe + " " + students[i].kommentar;
+
+            }
+        }
     }
 }
