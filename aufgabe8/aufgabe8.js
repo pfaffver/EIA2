350ca456bd492ae4dfec566c3a5d94abae4a4769
Verena <Verena@LAPTOP-T6V1EL0O.hfu-open.fu.hs-furtwangen.de>
2017-12-07 13:46:11 +0100

    aufgabe 8

--- aufgabe8/aufgabe8.js
+++ aufgabe8/aufgabe8.js
@@ -2,13 +2,32 @@ var aufgabe8;
 (function (aufgabe8) {
     window.addEventListener("load", init);
     function init() {
-        for (let i = 0; i < 10; i++) {
-            let h = document.createElement("h2");
-            h.innerText = "Hallo, ich bin h2-Element Nr." + i;
-            document.body.appendChild(h);
-            if (i == 3)
-                h.style.color = "red";
+        let anzahl = prompt("Gib hier ein, wie viele Quadrate du erzeugen willst");
+        let anzahlNeu = parseInt(anzahl);
+        let width = prompt("Gib hier ein, wie breit die Quadrate sein sollen");
+        let widthNeu = parseInt(width);
+        let height = prompt("Gib hier ein, wie hoch die Quadrate sein sollen");
+        let heightNeu = parseInt(height);
+        var x = 10;
+        var total = 100;
+        if (anzahlNeu >= x && anzahlNeu <= total) {
+            for (var i = 0; i < anzahlNeu; i++) {
+                drawSquare(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", widthNeu, heightNeu);
+            }
         }
+        else {
+            window.alert("Deine Anzahl liegt nicht zwischen 10 und 100\nn versuche es noch einmal");
+            init();
+        }
+    }
+    function drawSquare(_x, _y, _color, _width, _height) {
+        let quadrat = document.createElement("div");
+        quadrat.style.backgroundColor = _color;
+        quadrat.style.width = _width.toString();
+        quadrat.style.height = _height.toString();
+        quadrat.style.marginLeft = _x.toString();
+        quadrat.style.marginTop = _y.toString();
+        document.body.appendChild(quadrat);
     }
 })(aufgabe8 || (aufgabe8 = {}));
 //# sourceMappingURL=aufgabe8.js.map
--- aufgabe8/aufgabe8.ts
+++ aufgabe8/aufgabe8.ts
@@ -2,12 +2,38 @@ namespace aufgabe8 {
     window.addEventListener("load", init);
 
     function init(): void {
-        for (let i: number = 0; i < 10; i++) {
-            let h: HTMLHeadingElement = document.createElement("h2");
-            h.innerText = "Hallo, ich bin h2-Element Nr." + i;
-            document.body.appendChild(h);
-            if (i == 3) 
-                h.style.color = "red";
+        let anzahl: string = prompt("Gib hier ein, wie viele Quadrate du erzeugen willst");
+        let anzahlNeu: number = parseInt(anzahl);
+
+        let width: string = prompt("Gib hier ein, wie breit die Quadrate sein sollen");
+        let widthNeu: number = parseInt(width);
+
+        let height: string = prompt("Gib hier ein, wie hoch die Quadrate sein sollen");
+        let heightNeu: number = parseInt(height);
+
+        var x: number = 10;
+        var total: number = 100;
+        if (anzahlNeu >= x && anzahlNeu <= total) {
+            for (var i: number = 0; i < anzahlNeu; i++) {
+                drawSquare(Math.random() * 800, Math.random() * 600, "hsl(" + Math.random() * 360 + ", 100%, 50%)", widthNeu, heightNeu);
+            }
         }
+        else {
+            window.alert("Deine Anzahl liegt nicht zwischen 10 und 100\nn versuche es noch einmal");
+            init();
+        }
+    }
+
+    function drawSquare(_x: number, _y: number, _color: string, _width: number, _height: number): void {
+
+        let quadrat: HTMLDivElement = document.createElement("div");
+
+        quadrat.style.backgroundColor = _color;
+        quadrat.style.width = _width.toString();
+        quadrat.style.height = _height.toString();
+        quadrat.style.marginLeft = _x.toString();
+        quadrat.style.marginTop = _y.toString();
+
+        document.body.appendChild(quadrat);
     }
 }
