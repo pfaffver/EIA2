fd8e8fffd150cb9710f8af6c82b16eebf42cd3ac
Verena <Verena@LAPTOP-T6V1EL0O.eduroam.hs-furtwangen.de>
2017-12-15 10:55:12 +0100

    save 9

--- aufgabe9/aufgabe9.js
+++ aufgabe9/aufgabe9.js
@@ -2,6 +2,7 @@ var aufgabe9;
 (function (aufgabe9) {
     window.addEventListener("load", init);
     let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let saveLetter = "";
     function init() {
         for (let i = 0; i < letters.length; i++) {
             let l = document.createElement("div");
@@ -19,12 +20,23 @@ var aufgabe9;
         b.style.width = "85%";
         b.style.height = "50%";
         b.style.marginTop = "2em";
+        b.addEventListener("click", handleMouseClickInBox);
         document.body.appendChild(b);
     }
     function handleMouseClick(_event) {
         let d = _event.target;
+        console.log(_event.target);
         d.style.color = "grey";
         d.style.backgroundColor = "black";
+        saveLetter = d.innerText;
+    }
+    function handleMouseClickInBox(_event) {
+        let e = document.createElement("div");
+        e.innerText = saveLetter;
+        e.style.position = "absolute";
+        e.style.left = _event.pageX + "px";
+        e.style.top = _event.pageY + "px";
+        document.body.appendChild(e);
     }
 })(aufgabe9 || (aufgabe9 = {}));
 //# sourceMappingURL=aufgabe9.js.map
--- aufgabe9/aufgabe9.ts
+++ aufgabe9/aufgabe9.ts
@@ -3,6 +3,8 @@ namespace aufgabe9 {
 
     let letters: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+    let saveLetter: string = "";
+
     function init(): void {
         for (let i: number = 0; i < letters.length; i++) {
             let l: HTMLDivElement = document.createElement("div");
@@ -14,19 +16,31 @@ namespace aufgabe9 {
             l.addEventListener("click", handleMouseClick);
             document.body.appendChild(l);
         }
-        
+
         let b: HTMLDivElement = document.createElement("div");
         b.style.backgroundColor = "#F2F2F2";
         b.style.border = "3px solid lightgrey";
         b.style.width = "85%";
         b.style.height = "50%";
-        b.style.marginTop ="2em";
+        b.style.marginTop = "2em";
+        b.addEventListener("click", handleMouseClickInBox);
         document.body.appendChild(b);
     }
 
     function handleMouseClick(_event: MouseEvent): void {
-        let d: HTMLHeadingElement = <HTMLHeadingElement>_event.target;
+        let d: HTMLDivElement = <HTMLDivElement>_event.target;
+        console.log(_event.target)
         d.style.color = "grey";
         d.style.backgroundColor = "black";
+        saveLetter = d.innerText;
+    }
+
+    function handleMouseClickInBox(_event: MouseEvent): void {
+        let e: HTMLDivElement = document.createElement("div");
+        e.innerText = saveLetter;
+        e.style.position = "absolute";
+        e.style.left = _event.pageX + "px";
+        e.style.top = _event.pageY + "px";
+        document.body.appendChild(e);
     }
 }
