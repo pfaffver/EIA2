c0fad76b7979ec4bf982061bae6e6683501b3784
Verena <Verena@LAPTOP-T6V1EL0O>
2017-12-16 14:52:15 +0100

    AD aufg. 9

--- aufgabe6/DellOro/Circle.js
+++ aufgabe6/DellOro/Circle.js
@@ -1,1 +1,34 @@
+var L6_InheritanceCommonSuperclass;
+(function (L6_InheritanceCommonSuperclass) {
+    class Circle extends L6_InheritanceCommonSuperclass.MovingShape {
+        constructor(_x, _y) {
+            super(_x, _y);
+            console.log("Create circle");
+            this.speed = 0.1;
+            this.setRandomPosition(); // x und y werden neu definiert
+            this.setRandomTargetPosition();
+        }
+        setRandomTargetPosition() {
+            this.xTarget = Math.random() * L6_InheritanceCommonSuperclass.crc2.canvas.width;
+            this.yTarget = Math.random() * L6_InheritanceCommonSuperclass.crc2.canvas.height;
+        }
+        draw() {
+            L6_InheritanceCommonSuperclass.crc2.fillStyle = this.color;
+            L6_InheritanceCommonSuperclass.crc2.beginPath();
+            L6_InheritanceCommonSuperclass.crc2.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+            L6_InheritanceCommonSuperclass.crc2.fill();
+        }
+        move() {
+            let xDiff = this.xTarget - this.x;
+            let yDiff = this.yTarget - this.y;
+            if (Math.abs(xDiff) < 1 && Math.abs(yDiff) < 1)
+                this.setRandomTargetPosition();
+            else {
+                this.x += xDiff * this.speed;
+                this.y += yDiff * this.speed;
+            }
+        }
+    }
+    L6_InheritanceCommonSuperclass.Circle = Circle;
+})(L6_InheritanceCommonSuperclass || (L6_InheritanceCommonSuperclass = {}));
 //# sourceMappingURL=Circle.js.map
--- aufgabe6/DellOro/Main.js
+++ aufgabe6/DellOro/Main.js
@@ -1,1 +1,29 @@
+var L6_InheritanceCommonSuperclass;
+(function (L6_InheritanceCommonSuperclass) {
+    let shapes = [];
+    let n = 10;
+    window.addEventListener("load", init);
+    function init(_event) {
+        let canvas;
+        canvas = document.getElementsByTagName("canvas")[0];
+        L6_InheritanceCommonSuperclass.crc2 = canvas.getContext("2d");
+        for (let i = 0; i < n; i++) {
+            let s = new L6_InheritanceCommonSuperclass.Square(300, 150);
+            shapes.push(s);
+            let c = new L6_InheritanceCommonSuperclass.Circle(100, 150);
+            shapes.push(c);
+        }
+        console.log(shapes);
+        animate();
+    }
+    function animate() {
+        L6_InheritanceCommonSuperclass.crc2.fillStyle = "black";
+        L6_InheritanceCommonSuperclass.crc2.fillRect(0, 0, L6_InheritanceCommonSuperclass.crc2.canvas.width, L6_InheritanceCommonSuperclass.crc2.canvas.height);
+        for (let i = 0; i < shapes.length; i++) {
+            let s = shapes[i];
+            s.update();
+        }
+        window.setTimeout(animate, 20);
+    }
+})(L6_InheritanceCommonSuperclass || (L6_InheritanceCommonSuperclass = {}));
 //# sourceMappingURL=Main.js.map
