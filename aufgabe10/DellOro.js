750d8fd89e53b3252316b1c7772af60117994f10
Verena <Verena@LAPTOP-T6V1EL0O.hfu-open.fu.hs-furtwangen.de>
2017-12-21 12:30:03 +0100

    Delloro Code

---
+++ aufgabe10/DellOro.js
@@ -0,0 +1,36 @@
+var L09_FormElements;
+(function (L09_FormElements) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Init");
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+        }
+    }
+    function handleChange(_event) {
+        //console.log(_event);
+        //*/
+        let target = _event.target;
+        console.log("Changed " + target.name + " to " + target.value);
+        //*/
+        //*/ note: this == _event.currentTarget in an event-handler
+        if (this.id == "checkbox")
+            console.log("Changed " + target.name + " to " + target.checked);
+        //*/
+        //*/
+        if (target.name == "Slider") {
+            let progress = document.getElementsByTagName("progress")[0];
+            progress.value = parseFloat(target.value);
+        }
+        //*/
+        //*/
+        if (target.name == "Stepper") {
+            let progress = document.getElementsByTagName("meter")[0];
+            progress.value = parseFloat(target.value);
+        }
+        //*/
+    }
+})(L09_FormElements || (L09_FormElements = {}));
+//# sourceMappingURL=DellOro.js.map
