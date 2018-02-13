7d71326ed90b45bb03a7345f2a17835086237f42
Verena <Verena@LAPTOP-T6V1EL0O.hfu-open.fu.hs-furtwangen.de>
2018-02-13 16:38:54 +0100

    final

--- Final/final.js
+++ Final/final.js
@@ -1,23 +1,48 @@
 var final;
 (function (final) {
     window.addEventListener("load", createPicture);
-    let Inhalt;
+    let Inhalt = [];
     let InhaltFehler;
+    let random = Math.round(Math.random() * 10);
     function createPicture() {
-        // erstes Canvas
-        let canvas = document.getElementsByTagName("canvas")[0];
-        console.log(canvas);
+        for (let i = 0; i < 10; i++) {
+            if (random != i) {
+                let inhalt = document.getElementById("content");
+                let b = document.createElement("canvas");
+                b.width = 100;
+                b.height = 100;
+                b.addEventListener("click", falsch);
+                document.body.appendChild(b);
+            }
+            else {
+                let inhalt = document.getElementById("content");
+                let b = document.createElement("canvas");
+                b.width = 100;
+                b.height = 100;
+                b.addEventListener("click", richtig);
+                document.body.appendChild(b);
+            }
+        }
+        for (let i = 0; i < 10; i++) {
+            if (random != i) {
+                let canvas = document.getElementsByTagName("canvas")[i];
+                final.can = canvas.getContext("2d");
+                console.log(canvas);
+                Inhalt[i] = new final.bild(0, 0);
+                Inhalt[i].draw();
+            }
+        }
+        let canvas = document.getElementsByTagName("canvas")[random];
         final.can = canvas.getContext("2d");
-        console.log(final.can);
-        Inhalt = new final.bild(0, 0);
-        Inhalt.draw();
-        // zweites Canvas
-        let canvas2 = document.getElementsByTagName("canvas")[1];
-        console.log(canvas);
-        final.can = canvas2.getContext("2d");
         console.log(final.can);
         InhaltFehler = new final.bildFehler(0, 0);
         InhaltFehler.draw();
+    }
+    function falsch() {
+        alert("Leider falsch - Das ist nicht das gesuchte Fehlerbild");
+    }
+    function richtig() {
+        alert("RICHTIG - Du hast das Fehlerbild entdeckt");
     }
 })(final || (final = {}));
 //# sourceMappingURL=final.js.map
--- Final/final.ts
+++ Final/final.ts
@@ -3,28 +3,56 @@ namespace final {
     window.addEventListener("load", createPicture);
 
     export let can: CanvasRenderingContext2D;
-    let Inhalt: bild;
+    let Inhalt: bild[] = [];
     let InhaltFehler: bildFehler;
-
+    let random: number = Math.round(Math.random() * 10);
     function createPicture(): void {
-        // erstes Canvas
-        let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
-        console.log(canvas);
+
+        for (let i: number = 0; i < 10; i++) {
+            if (random != i) {
+                let inhalt: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+                let b: HTMLCanvasElement = document.createElement("canvas");
+                b.width = 100;
+                b.height = 100;
+                b.addEventListener("click", falsch);
+                document.body.appendChild(b);
+            }
+            else {
+                let inhalt: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+                let b: HTMLCanvasElement = document.createElement("canvas");
+                b.width = 100;
+                b.height = 100;
+                b.addEventListener("click", richtig);
+                document.body.appendChild(b);
+            }
+        }
+        
+        for (let i: number = 0; i < 10; i++) {
+            if (random != i) {
+                let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[i];
+                can = canvas.getContext("2d");
+                console.log(canvas);
+                Inhalt[i] = new bild(0, 0);
+                Inhalt[i].draw();
+            }
+        }
+
+        let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[random];
 
         can = canvas.getContext("2d");
-        console.log(can);
-
-        Inhalt = new bild(0, 0);
-        Inhalt.draw();
-        
-        // zweites Canvas
-        let canvas2: HTMLCanvasElement = document.getElementsByTagName("canvas")[1];
-        console.log(canvas);
-
-        can = canvas2.getContext("2d");
         console.log(can);
 
         InhaltFehler = new bildFehler(0, 0);
         InhaltFehler.draw();
     }
+    
+    function falsch(): void {
+        alert("Leider falsch - Das ist nicht das gesuchte Fehlerbild");
+    }
+    
+    function richtig(): void {
+        alert("RICHTIG - Du hast das Fehlerbild entdeckt");
+    }
+    
+    
 }
