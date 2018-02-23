865a686a5ab8bd3a6db5472e3b1651d6f846d077
Verena <Verena@LAPTOP-T6V1EL0O.fritz.box>
2018-02-23 18:57:25 +0100

    finalNEU

---
+++ FinalNeu/bild.ts
@@ -0,0 +1,130 @@
+/* Endaufgabe
+Name: Verena Pfaff 
+Matrikel: 256543
+Datum: 13.02.2018 */
+
+namespace finalaufgabe {
+
+    export class bild {
+        x: number;
+        y: number;
+        color: string;
+
+        constructor(_x: number, _y: number, _color: string) {
+            this.x = _x;
+            this.y = _y;
+            this.color = _color;
+        }
+
+        draw(): void {
+            //Hintergrund
+            can.fillStyle = "#E3CEF6";
+            can.fillRect(this.x, this.y, 150, 100);
+
+            //Waffel
+            can.beginPath();
+            can.moveTo(this.x + 65, this.y + 50);
+            can.lineTo(this.x + 35, this.y + 50);
+            can.lineTo(this.x + 50, this.y + 90);
+            can.closePath();
+            can.fillStyle = "#8A4B08";
+            can.fill();
+            
+            //Waffelstriche von rechts nach links
+            can.beginPath();
+            can.moveTo(this.x + 40, this.y + 60);
+            can.lineTo(this.x + 65, this.y + 50);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 41, this.y + 65);
+            can.lineTo(this.x + 65, this.y + 55);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 43, this.y + 70);
+            can.lineTo(this.x + 60, this.y + 62);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 45, this.y + 75);
+            can.lineTo(this.x + 58, this.y + 69);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 47, this.y + 80);
+            can.lineTo(this.x + 55, this.y + 76);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            //Waffelstriche von links nach rechts
+            can.beginPath();
+            can.moveTo(this.x + 35, this.y + 55);
+            can.lineTo(this.x + 60, this.y + 62);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 40, this.y + 62);
+            can.lineTo(this.x + 58, this.y + 68);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 45, this.y + 70);
+            can.lineTo(this.x + 56, this.y + 74);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+            
+            can.beginPath();
+            can.moveTo(this.x + 46, this.y + 75);
+            can.lineTo(this.x + 54, this.y + 79);
+            can.strokeStyle = "#61210B";
+            can.stroke();
+                        
+            //Schirmchen
+            can.beginPath();
+            can.moveTo(this.x + 55, this.y + 50);
+            can.lineTo(this.x + 80, this.y + 10);
+            can.strokeStyle = "#2E64FE";
+            can.stroke();
+
+            can.beginPath();
+            can.moveTo(this.x + 80, this.y + 10);
+            can.lineTo(this.x + 70, this.y + 10);
+            can.lineTo(this.x + 85, this.y + 20);
+            can.closePath();
+            can.stroke();
+            can.fillStyle = "#2E64FE";
+            can.fill();
+
+            //Schirmchen Spitze
+            can.beginPath();
+            can.arc(this.x + 80, this.y + 10, 2, 0, 2 * Math.PI);
+            can.fillStyle = "#2E64FE";
+            can.fill();
+
+            //Erdbeere
+            can.beginPath();
+            can.arc(this.x + 40, this.y + 45, 12, 0, 2 * Math.PI);
+            can.fillStyle = this.color;
+            can.fill();
+
+            //Vanille
+            can.beginPath();
+            can.arc(this.x + 60, this.y + 45, 12, 0, 2 * Math.PI);
+            can.fillStyle = "#F7FE2E";
+            can.fill();
+
+            //Schokolade
+            can.beginPath();
+            can.arc(this.x + 50, this.y + 30, 12, 0, 2 * Math.PI);
+            can.fillStyle = "#3B240B";
+            can.fill();          
+        }
+    }
+}
---
+++ FinalNeu/final.ts
@@ -0,0 +1,116 @@
+/* Endaufgabe
+Name: Verena Pfaff 
+Matrikel: 256543
+Datum: 13.02.2018 */
+
+namespace finalaufgabe {
+
+    window.addEventListener("load", createPicture);
+    let clickCounter: number = 0;
+
+    export let can: CanvasRenderingContext2D;
+    let Inhalt: bild[] = [];
+    let InhaltFehler: bildFehler;
+    let bilder: number =3;
+    let fehlerText: string[] = ["Leider Falsch", "So schwer ist es doch nicht...", "Hast du was an den Augen?"];
+    let textAus: number = 0;
+    let random: number = Math.round(Math.random() * bilder);
+
+    function createPicture(): void {
+
+        for (let i: number = 0; i < bilder+1; i++) {
+            if (random != i) {
+                let inhalt: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+                let b: HTMLCanvasElement = document.createElement("canvas");
+                b.width = 100;
+                b.height = 100;
+                b.id = "canvas"+i;
+                b.addEventListener("click", falsch);
+                document.body.appendChild(b);
+            }
+
+            else {
+                let inhalt: HTMLDivElement = <HTMLDivElement>document.getElementById("content");
+                let b: HTMLCanvasElement = document.createElement("canvas");
+                b.width = 100;
+                b.height = 100;
+                b.id = "canvas"+i;
+                b.addEventListener("click", richtig);
+                document.body.appendChild(b);
+            }
+        }
+
+        for (let i: number = 0; i < bilder+1; i++) {
+            if (random != i) {
+                let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[i];
+                can = canvas.getContext("2d");
+                console.log(canvas);
+                Inhalt[i] = new bild(0, 0, "#FA5858");
+                Inhalt[i].draw();
+            }
+        }
+
+        let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[random];
+
+        can = canvas.getContext("2d");
+        console.log(can);
+
+        InhaltFehler = new bildFehler(0, 0, "#FE2E2E");
+        InhaltFehler.draw();
+    }
+
+    function falsch(): void {
+        clickCounter = clickCounter + 1;
+        
+        let tonFalse: HTMLMediaElement = <HTMLMediaElement>document.getElementById("plop");
+            tonFalse.play();      
+          
+        if (textAus < 3) {            
+            alert(fehlerText[textAus]);
+            textAus++;
+        }
+
+        else {
+            alert("Es ist das " + (random + 1) + " Bild - Die Erdbeerkugel hat eine andere Farbe");
+        }
+    }
+
+    function richtig(): void {
+        clickCounter = clickCounter + 1;
+
+        for (let i: number = 0; i < bilder+1; i++) {
+            document.getElementById("canvas"+i).parentElement.removeChild(document.getElementById("canvas"+i));
+        }
+
+        let b: HTMLDivElement = document.createElement("div");
+        b.id = "schluss";
+        b.innerText = "Sehr Gut! Du hast das gesuchte Bild gefunden und dabei " + clickCounter + " Versuch(e) gebraucht";
+        document.body.appendChild(b);
+
+        let a: HTMLImageElement = document.createElement("img");
+        a.src = "congrats.gif";
+        a.id = "congrats";
+        document.body.appendChild(a);
+
+        let tonCelebration: HTMLMediaElement = <HTMLMediaElement>document.getElementById("gold");
+        tonCelebration.play();
+        
+         let c: HTMLButtonElement = document.createElement("button");
+        c.innerText = "Nächstes Level";
+        c.id = "nl";
+        c.style.marginLeft = "30%";
+        c.style.width = "40%";
+        c.style.backgroundColor = "pink";
+        c.addEventListener("click",nlAufruf);
+        document.body.appendChild(c);
+
+    }
+    function nlAufruf():void{
+       document.getElementById("schluss").parentNode.removeChild(document.getElementById("schluss"));
+        document.getElementById("congrats").parentNode.removeChild(document.getElementById("congrats"));
+         document.getElementById("nl").parentNode.removeChild(document.getElementById("nl"));
+       
+        bilder += 2;
+        createPicture();
+    }
+}
